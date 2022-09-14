1332ceb93c2d9c8ee44f33524e5fba9f0ce6117e
Chidi <rodneybrain@gmail.com>
2022-09-13 18:49:28 -0700

    commit 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
-* main - prints _putchar, follwed by a new line
-* Return: Always 0 (Success)
-*/
-int main (void)
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
 {
-	char cli []  = "_putchar";
+		_putchar('_');
+			_putchar('p');
+				_putchar('u');
+					_putchar('t');
+						_putchar('c');
+							_putchar('h');
+								_putchar('a');
+									_putchar('r');
+										_putchar('\n');
 
-	int c;
+											return (0);
 
-	for (c = 0; c < 9; c++)
-	{
-		_putchar(cli[c])
-	}
-	_putchar('\n');
-	return (0)
 }
