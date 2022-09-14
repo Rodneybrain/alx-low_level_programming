d09cca26fa3041dfe6d57576db3f36f37967f7f3
Chidi <rodneybrain@gmail.com>
2022-09-13 18:06:32 -0700

    commit 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main-prints_putchar, follwed by a new line
+*Return: Always 0 (Success)
+*/
+int main (void)
+{
+	char cli ||="_putchar:;
+
+	int c;
+
+	for (c = 0;c < 9; c++)
+	{
+		_putchar(cli[c}
+	}
+	_putchar('\n');
+	return (0)
+}
