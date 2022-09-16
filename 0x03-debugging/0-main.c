c1bed2eeb474a955d2f3ac778e6acd885f450a44
Chidi <rodneybrain@gmail.com>
2022-09-15 20:22:56 -0700

    commit 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+lude "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
